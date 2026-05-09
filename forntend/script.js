32802642ddd1f54b00ae3e8855452219b0b3cc6e
Rabi Kumar Mahato <rn9169811@gmail.com>
2026-05-09 16:16:08 +0530

    fixed frontend API URL

--- forntend/script.js
+++ forntend/script.js
@@ -87,7 +87,7 @@ document.getElementById('contactForm').addEventListener('submit', async function
   label.textContent = 'Sending…';
 
   try {
-    const res = await fetch("https://portfolio-backend-xneg.onrender.com/api/contact", {
+    fetch("https://portfolio-backend-xneg.onrender.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
